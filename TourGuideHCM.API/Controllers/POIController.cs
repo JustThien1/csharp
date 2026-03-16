c46a183568d29cb280eb529c98c8359995717b35
thien <thien@THIEN1>
2026-03-16 15:58:57 +0700

    Add project files.

---
+++ TourGuideHCM.API/Controllers/POIController.cs
@@ -0,0 +1,15 @@
+﻿namespace TourGuideHCM.API
+{
+    public class POI
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Radius { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string AudioUrl { get; set; }
+        public int Priority { get; set; }
+    }
+}
